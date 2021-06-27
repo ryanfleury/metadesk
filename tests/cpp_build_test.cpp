c3c863c7fa2b42fdfae07676b0f183374a08c6b4
ryanfleury <ryan.j.fleury@gmail.com>
2021-06-27 10:19:08 -0600

    c++ build test, add user-defined-literals for metadesk strings

--- tests/cpp_build_test.cpp
+++ tests/cpp_build_test.cpp
@@ -1,9 +1,16 @@
 #include "md.h"
 #include "md.c"
 
+MD_String8 operator "" _md(const char *s, size_t size)
+{
+    MD_String8 str = MD_S8((MD_u8 *)s, (MD_u64)size);
+    return str;
+}
+
 int main(void)
 {
-    
+    MD_String8 str = "foobar"_md;
+    printf("%.*s", MD_StringExpand(str));
     
     return 0;
 }
